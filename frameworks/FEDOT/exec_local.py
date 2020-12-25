fe4c52ab8272e262d499c24550b8e620e324970f
Nikolay Nikitin <nicl.nno@gmail.com>
2020-12-25 21:16:36 +0300

    For debug

--- frameworks/FEDOT/exec_local.py
+++ frameworks/FEDOT/exec_local.py
@@ -4,8 +4,7 @@ import pprint
 import sys
 import tempfile as tmp
 from copy import copy
-from sklearn.utils import shuffle
-from numpy import squeeze
+
 if sys.platform == 'darwin':
     os.environ['OBJC_DISABLE_INITIALIZE_FORK_SAFETY'] = 'YES'
 os.environ['JOBLIB_TEMP_FOLDER'] = tmp.gettempdir()
@@ -22,9 +21,103 @@ from fedot.core.composer.optimisers.gp_optimiser import GPChainOptimiserParamete
 from fedot.core.repository.tasks import Task, TaskTypesEnum
 from fedot.core.repository.dataset_types import DataTypesEnum
 from fedot.core.data.data import InputData
-from frameworks.shared.callee import call_run, result, output_subdir, utils
 import numpy as np
 import datetime
+
+import importlib.util
+import json
+import logging
+import os
+import re
+import sys
+from sklearn.utils import shuffle
+import numpy as np
+
+def load_module(name, path):
+    spec = importlib.util.spec_from_file_location(name, path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[name] = module
+    spec.loader.exec_module(module)
+    return module
+
+
+amlb_path = os.environ.get("AMLB_PATH")
+if amlb_path:
+    utils = load_module("amlb.utils", os.path.join(amlb_path, "utils", "__init__.py"))
+    NS = utils.Namespace
+    touch = utils.touch
+else:
+    from amlb.utils import Namespace as NS, touch
+
+
+def setup_logger():
+    console = logging.StreamHandler(sys.stdout)
+    console.setLevel(logging.INFO)
+    handlers = [console]
+    logging.basicConfig(handlers=handlers)
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+
+
+setup_logger()
+
+log = logging.getLogger(__name__)
+
+
+def result(output_file=None,
+           predictions=None, truth=None,
+           probabilities=None, probabilities_labels=None,
+           target_is_encoded=False,
+           error_message=None,
+           models_count=None,
+           training_duration=None,
+           **others):
+    return locals()
+
+
+def output_subdir(name, config):
+    subdir = os.path.join(config.output_dir, name, config.name, str(config.fold))
+    touch(subdir, as_dir=True)
+    return subdir
+
+
+data_keys = re.compile("^(X|y|data)(_.+)?$")
+
+
+def call_run(run_fn):
+    import numpy as np
+
+    params = NS.from_dict({"dataset":{"train":{"X_enc":"/tmp/iris/train.X_enc.npy","y_enc":"/tmp/iris/train.y_enc.npy"},"test":{"X_enc":"/tmp/iris/test.X_enc.npy","y_enc":"/tmp/iris/test.y_enc.npy"}},"config":{"framework":"FEDOT","framework_params":{},"type":"classification","name":"Australian","fold":0,"metrics":["auc","logloss","acc"],"metric":"auc","seed":3029240368,"max_runtime_seconds":600,"cores":4,"max_mem_size_mb":91763,"min_vol_size_mb":-1,"input_dir":"/home/rosneft_user_2500/.openml/cache","output_dir":"/home/rosneft_user_2500/bench/automlbenchmark/results/fedot.small.test.local.20201225T163641","output_predictions_file":"/home/rosneft_user_2500/bench/automlbenchmark/results/fedot.small.test.local.20201225T163641/predictions/fedot.Australian.0.csv","result_token":"5e433616-46cf-11eb-a671-7957e32fc18d","result_dir":"/tmp/iris"}})
+
+    def load_data(name, path, **ignored):
+        if isinstance(path, str) and data_keys.match(name):
+            return name, np.load(path, allow_pickle=True)
+        return name, path
+
+    print(params.dataset)
+    ds = NS.walk(params.dataset, load_data)
+
+    config = params.config
+    config.framework_params = NS.dict(config.framework_params)
+
+    try:
+        result = run_fn(ds, config)
+        res = dict(result)
+        for name in ['predictions', 'truth', 'probabilities']:
+            arr = result[name]
+            if arr is not None:
+                res[name] = os.path.join(config.result_dir, '.'.join([name, 'npy']))
+                np.save(res[name], arr, allow_pickle=True)
+    except Exception as e:
+        log.exception(e)
+        res = dict(
+            error_message=str(e),
+            models_count=0
+        )
+
+    print(config.result_token)
+    print(json.dumps(res, separators=(',', ':')))
+
 
 log = logging.getLogger(__name__)
 
@@ -58,19 +151,15 @@ def run(dataset, config):
 
     task = Task(task_type)
 
-    x_train, y_train = shuffle(dataset.train.X_enc, dataset.train.y_enc, random_state=0)
-
-    if len(y_train.shape) > 1 and y_train.shape[1] == 1:
-        y_train = squeeze(y_train, axis=1)
-
-    import numpy as np
-    # if len(np.unique(y_train)) < 3:
-    #    return None
-    # else:
-    #    while True:
-    #        print("!")
+    x_train = dataset.train.X_enc
+    y_train = dataset.train.y_enc
 
     x_test = dataset.test.X_enc
+
+    x_train, y_train = shuffle(dataset.train.X_enc, dataset.train.y_enc, random_state = 0)
+
+    if len(y_train.shape) > 1 and y_train.shape[1] == 1:
+        y_train = np.squeeze(y_train, axis=1)
 
     training_params = {k: v for k, v in config.framework_params.items() if not k.startswith('_')}
 
@@ -102,29 +191,29 @@ def run(dataset, config):
     # Create GP-based composer
     composer = GPComposer()
 
-    with utils.Timer() as training:
-        # the choice and initialisation of the GP search
-        composer_requirements = GPComposerRequirements(
-            primary=available_model_types,
-            secondary=available_model_types, max_arity=3,
-            max_depth=3, pop_size=20, num_of_generations=1,
-            crossover_prob=0.8, mutation_prob=0.8, max_lead_time=datetime.timedelta(minutes=runtime_min))
+    #with utils.Timer() as training:
+    # the choice and initialisation of the GP search
+    composer_requirements = GPComposerRequirements(
+        primary=available_model_types,
+        secondary=available_model_types, max_arity=3,
+        max_depth=3, pop_size=3, num_of_generations=2,
+        crossover_prob=0.8, mutation_prob=0.8, max_lead_time=datetime.timedelta(minutes=runtime_min))
 
-        # GP optimiser parameters choice
-        scheme_type = GeneticSchemeTypesEnum.steady_state
-        optimiser_parameters = GPChainOptimiserParameters(genetic_scheme_type=scheme_type)
+    # GP optimiser parameters choice
+    scheme_type = GeneticSchemeTypesEnum.steady_state
+    optimiser_parameters = GPChainOptimiserParameters(genetic_scheme_type=scheme_type)
 
-        # Create builder for composer and set composer params
-        builder = GPComposerBuilder(task=task).with_requirements(composer_requirements).with_metrics(
-            metric_function).with_optimiser_parameters(optimiser_parameters)
+    # Create builder for composer and set composer params
+    builder = GPComposerBuilder(task=task).with_requirements(composer_requirements).with_metrics(
+        metric_function).with_optimiser_parameters(optimiser_parameters)
 
-        composer = builder.build()
+    composer = builder.build()
 
-        # the optimal chain generation by composition - the most time-consuming task
-        chain_evo_composed = composer.compose_chain(data=dataset_to_compose,
-                                                    is_visualise=False)
+    # the optimal chain generation by composition - the most time-consuming task
+    chain_evo_composed = composer.compose_chain(data=dataset_to_compose,
+                                                is_visualise=False)
 
-        chain_evo_composed.fit(input_data=dataset_to_compose, verbose=False)
+    chain_evo_composed.fit(input_data=dataset_to_compose, verbose=False)
 
     log.info('Predicting on the test set.')
     y_test = dataset.test.y_enc
