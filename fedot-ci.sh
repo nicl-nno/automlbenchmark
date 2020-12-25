2b46bbb827c53fbe17b4f863540fd1c0da4e9a0a
Nikolay Nikitin <nicl.nno@gmail.com>
2020-12-25 18:47:49 +0300

    CI script added

---
+++ fedot-ci.sh
@@ -0,0 +1,19 @@
+echo "Start FEDOT benchmarking"
+
+## Update FEDOT ##
+./frameworks/FEDOT/setup.sh
+
+## Run benchmarks ##
+python3 runbenchmark.py FEDOT test
+
+## Get result ##
+cd results
+latest_results=$(ls -td -- */ | grep fedot | head -n 1)
+echo $latest_results
+cp $latest_results ~/nfs/40/share_0/fedot-daily-benchmarks/$latest_results -r
+cd ..
+
+echo Finished
+
+
+
